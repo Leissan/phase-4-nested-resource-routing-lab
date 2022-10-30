f9bc71ce82e29570b7a5c18bb0cec83e2168ecca
Leissan Khan <leissan.khan@gmail.com>
2022-10-30 17:18:33 -0400

    commit tree dfbf2e21a214cc00a11f0da944e11969a8dd4c18

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,31 @@
 class ItemsController < ApplicationController
 
   def index
+    if params[:user_id]
+    user = User.find_by(params[:user_id])
+    render json: user.items, include: :user
+   
+    else
     items = Item.all
     render json: items, include: :user
+    end
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create 
+    if params[:user_id]
+      user = User.find_by(params[:user_id])
+      item = user.items.build(name: "Garden gnomes", description: "No refunds", price: 23)
+      render json: item, include: :user, status: :created
+    else
+      item = Item.create(name: "Garden gnomes", description: "No refunds", price: 23)
+      render json: item, include: :user, status: :created
+    end
+    item
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  
+  resources :users, only: [:show] do
+    resources :items, only: [:show, :index, :create] #routes for nested resources
+  end
+
+  resources :items, only: [:show, :index, :create] #anything non-nested
 end
